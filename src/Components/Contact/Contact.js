5f7ceb45e7334bb5241e3d0a7761e5f1f95f6bdf
Rayan Encarnacion <rayanbalbuena@gmail.com>
2022-01-04 12:29:22 -0400

    Email validation added

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,45 +1,67 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import env from "react-dotenv";
 import AccentAnchor from "../../UI/AccentAnchor";
 import SectionWrapper from "../../UI/SectionWrapper";
-import sendEmail from "./send-email";
 import StyledForm from "./Styled/StyledForm";
+import useContact from "./use-contact";
+
+const invalidEmail = (
+  <small style={{ color: "#ff7d7d", textAlign: "left", marginBottom: ".8em" }}>
+    Enter a valid email address!
+  </small>
+);
 
 const Contact = () => {
-  const [sendingDataError, setSendingDataError] = useState(false);
-  const [dataSentSuccess, setDataSentSuccess] = useState(false);
-  const formRef = useRef();
-
-  const sendEmailHandler = (e) => {
-    e.preventDefault();
-    sendEmail(
-      formRef,
-      env.EMAIL_JS_TOKEN,
-      setDataSentSuccess,
-      setSendingDataError
-    );
-  };
+  const {
+    formRef,
+    error,
+    success,
+    emailIsValid,
+    enteredEmail,
+    onBlur,
+    onChange,
+    sendEmailHandler,
+  } = useContact();
 
   return (
     <SectionWrapper centered={true} id={"contact"} section={true}>
       <p className="accent">Contact</p>
       <h3>Get in touch with me</h3>
-      <StyledForm onSubmit={sendEmailHandler} ref={formRef} id="contact-form">
+      <StyledForm
+        onSubmit={(e) => sendEmailHandler(e, env.EMAIL_JS_TOKEN)}
+        ref={formRef}
+        id="contact-form"
+      >
         <h4>Send me a message</h4>
         <div className="inputs-container">
           <input type="hidden" name="contact_number" />
           <label htmlFor="name">Name</label>
           <input className="fields" required type="text" name="user_name" />
-          <label htmlFor="email">Email</label>
-          <input className="fields" required type="text" name="user_email" />
+          <label
+            style={{ color: `${emailIsValid ? "#f7f7f7" : "#ff7d7d"}` }}
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            style={{
+              borderBottomColor: `${emailIsValid ? "#f7f7f7" : "#ff7d7d"}`,
+            }}
+            className="fields"
+            required
+            type="text"
+            name="user_email"
+            value={enteredEmail}
+            onChange={onChange}
+            onBlur={onBlur}
+          />
+          {!emailIsValid && invalidEmail}
           <label htmlFor="message">Message</label>
           <input className="fields" required type="text" name="message" />
-          {dataSentSuccess && (
+          {success && (
             <p className="success-text">Message was sent succesfully!</p>
           )}
-          {sendingDataError && (
-            <p className="error-text">Error sending your message!</p>
-          )}
+          {error && <p className="error-text">Error sending your message!</p>}
         </div>
         <AccentAnchor as="button" light="true">
           Send
