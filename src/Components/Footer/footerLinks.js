07694543332b3b0ff8ffe9689b30a3070a95800f
Rayan Encarnacion <rayanbalbuena@gmail.com>
2022-03-30 13:49:32 -0400

    Debugging case sensitive issue with netlify # 1

---
+++ src/Components/Footer/footerLinks.js
@@ -0,0 +1,20 @@
+const footerLinks = [
+  {
+    text: "Home",
+    href: "#home",
+    id: Math.random(),
+  },
+  {
+    text: "About Me",
+    href: "#about",
+    id: Math.random(),
+  },
+  {
+    text: "Projects",
+    href: "#projects",
+
+    id: Math.random(),
+  },
+];
+
+export default footerLinks;
